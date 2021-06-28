f53d213caabc20429cae25e76f696f801cf58151
Frederik <frederik.borck@googlemail.com>
2021-06-28 21:28:00 +0200

    SF-094 change alliance style

--- resources/views/layouts/alliance.blade.php
+++ resources/views/layouts/alliance.blade.php
@@ -2,24 +2,24 @@
     <div class="row">
         @if($alliance->id != null)
             <div class="col-12 title-line">Allianz "{{$alliance->alliance_name}}"</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;">Allianz Tag</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;">{{$alliance->alliance_tag}}</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;">Mitglieder</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;">{{$alliance->members}} (<a href="/alliance/{{$activePlanet}}/memberslist/{{$alliance->alliance_id}}">Liste</a>)</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;">Gründer</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;"><a href="/profile/{{$alliance->founder_id}}">{{$alliance->nickname}}</a></div>
-            <div class="col-6 sub-line" style="margin-top: 1px;">Kampfstatistik</div>
-            <div class="col-6 sub-line" style="margin-top: 1px;"><a href="/statistics/{{$activePlanet}}/ally/{{$alliance->alliance_id}}">ansehen</a></div>
-            <div class="col-12 title-line" style="margin-top: 1px;">Allianzlogo</div>
-            <div class="col-12 sub-line" style="margin-top: 1px;">
+            <div class="col-6 sub-line">Allianz Tag</div>
+            <div class="col-6 sub-line">{{$alliance->alliance_tag}}</div>
+            <div class="col-6 sub-line">Mitglieder</div>
+            <div class="col-6 sub-line">{{$alliance->members}} (<a href="/alliance/{{$activePlanet}}/memberslist/{{$alliance->alliance_id}}">Liste</a>)</div>
+            <div class="col-6 sub-line">Gründer</div>
+            <div class="col-6 sub-line"><a href="/profile/{{$alliance->founder_id}}">{{$alliance->nickname}}</a></div>
+            <div class="col-6 sub-line">Kampfstatistik</div>
+            <div class="col-6 sub-line"><a href="/statistics/{{$activePlanet}}/ally/{{$alliance->alliance_id}}">ansehen</a></div>
+            <div class="col-12 title-line mt-3">Allianzlogo</div>
+            <div class="col-12 sub-line">
                 @if($alliance->alliance_logo != null)
                     <img class="img-fluid" src="{{$alliance->alliance_logo}}" alt="{{$alliance->alliance_name}} Logo"/>
                     @else
                     - keins -
                 @endif
             </div>
-            <div class="col-12 title-line" style="margin-top: 1px;">Allianzbeschreibung</div>
-            <div class="col-12 sub-line" style="margin-top: 1px;">
+            <div class="col-12 title-line mt-3">Allianzbeschreibung</div>
+            <div class="col-12 sub-line">
                 <div class="row">
                     <div class="col-12 col-sm-10 offset-sm-1">
                         @if($alliance->alliance_description != null)
@@ -33,7 +33,7 @@
                 </div>
             </div>
             @if($alliance->own)
-                <div class="col-12 title-line" style="margin-top: 1px;">Allianznachrichten</div>
+                <div class="col-12 title-line mt-3">Allianznachrichten</div>
                 @if($alliance->alliance_messages != null)
                     @foreach(json_decode($alliance->alliance_messages) as $message)
                         <div class="col-6 sub-line text-left">{{date("H:i:s - d.m.Y", $message->date)}} - <a href="/profile/{{$message->user_id}}">{{$message->user_name}} sagt:</a></div>
@@ -41,9 +41,9 @@
                         <div class="col-12 sub-line text-left">{{{$message->message}}}</div>
                     @endforeach
                 @else
-                    <div class="col-12 sub-line" style="margin-top: 1px;">- keine -</div>
+                    <div class="col-12 sub-line">- keine -</div>
                 @endif
-                <div class="col-12 title-line" style="margin-top: 1px;">Neue Nachricht</div>
+                <div class="col-12 title-line mt-3">Neue Nachricht</div>
                 <div class="col-12 sub-line">
                     <form class="col-12 col-sm-6 offset-sm-3" action="/alliance/{{$activePlanet}}/send/{{$alliance->alliance_id}}" method="post">
                         @csrf
@@ -59,8 +59,8 @@
                     </form>
                 </div>
                 @if($alliance->founder_id == $userData->user_id)
-                    <div class="col-6 sub-line p-1" style="margin-top: 1px;">Bewerbungen:</div>
-                    <div class="col-6 sub-line p-1" style="margin-top: 1px;">
+                    <div class="col-6 sub-line p-1">Bewerbungen:</div>
+                    <div class="col-6 sub-line p-1">
                         @if(count($applications) > 0)
                             <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#applicationCollapse" aria-expanded="false" aria-controls="applicationCollapse">({{count($applications)}}) Anzeigen</button>
                         @else
